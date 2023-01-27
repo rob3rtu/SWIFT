91059859fd2ccd448a88b14ef6597265e9067f3d
rob3rtu <robertudrea2002@yahoo.ro>
2023-01-27 22:11:44 +0200

    Programmatic project setup

---
+++ CardWorkout-Programmatic/CardWorkout-Programmatic/CardSelectionVC.swift
@@ -0,0 +1,19 @@
+//
+//  CardSelectionVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Robert Udrea on 27.01.2023.
+//
+
+import UIKit
+
+class CardSelectionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .brown
+    }
+    
+
+}
