b1713e412ef3c56a66d1ca3707b25faf426dfd52
rob3rtu <robertudrea2002@yahoo.ro>
2023-01-27 21:56:28 +0200

    Added some ios15 style on buttons

--- CardWorkout/CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardWorkout/CardSelectionVC.swift
@@ -10,7 +10,6 @@ import UIKit
 class CardSelectionVC: UIViewController {
 
     @IBOutlet var cardImageView: UIImageView!
-    @IBOutlet var buttons: [UIButton]!
     
     var timer: Timer!
     var cards: [UIImage] = Deck.allValues
@@ -18,9 +17,6 @@ class CardSelectionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         startTimer()
-        for button in buttons {
-            button.layer.cornerRadius = 8
-        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
