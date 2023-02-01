2d068f926d294c501a73f2dbb38a4ac169ff5917
rob3rtu <robertudrea2002@yahoo.ro>
2023-02-01 21:41:41 +0200

    Working app, to do some refactor asap

--- ExchangeApp/ExchangeApp/APIHandler.swift
+++ ExchangeApp/ExchangeApp/APIHandler.swift
@@ -8,7 +8,83 @@
 import UIKit
 
 class APIHandler {
+    public var symbols: [String] = []
+    public var convertedValue: Float = 0
+    
     init() {
         //make the request
+        let url = URL(string: "https://api.exchangerate.host/symbols")
+        URLSession.shared.dataTask(with: url!, completionHandler: {data, response, error in
+            guard let data = data, error == nil else {
+                print("Something went wrong!")
+                return
+            }
+            
+            var result:Response?
+            
+            do{
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }catch {
+                print("Error converting the result")
+            }
+            
+            guard let result = result else {
+                print("no result")
+                return
+            }
+            
+            print(result.symbols.keys)
+            
+            for key in result.symbols.keys {
+                self.symbols.append(key)
+            }
+            
+        }).resume()
     }
+    
+    public func convert(from: String, to: String, amount: Float) {
+        let url = URL(string: "https://api.exchangerate.host/convert?from=\(from)&to=\(to)&amount=\(amount)&source=bnro&places=1")
+        
+        URLSession.shared.dataTask(with: url!, completionHandler: {data, response, error in
+            guard let data = data, error == nil else {
+                print("Error on conversion")
+                return
+            }
+            
+            var result: Conversion?
+            
+            do{
+                result = try JSONDecoder().decode(Conversion.self, from: data)
+            }catch {
+                print("Error converting the result")
+            }
+            
+            guard let result = result else {
+                print("no result")
+                return
+            }
+            
+            print("Converted value")
+            print(result.result)
+            
+            self.convertedValue = result.result
+            
+        }).resume()
+    }
+    
+}
+
+struct Conversion: Codable {
+    let success: Bool
+    let result: Float
+}
+
+struct Response: Codable {
+    let success: Bool
+    let symbols: [String:symbol]
+}
+
+struct symbol: Codable {
+    let description: String
+    let code: String
 }
--- ExchangeApp/ExchangeApp/HomePageVC.swift
+++ ExchangeApp/ExchangeApp/HomePageVC.swift
@@ -9,47 +9,94 @@ import UIKit
 
 class HomePageVC: UIViewController {
     @IBOutlet var inputField: UITextField!
-    
     @IBOutlet var menuTo: UIButton!
     @IBOutlet var menuFrom: UIButton!
-    
     @IBOutlet var convertButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
+
+    var initial: Float          = 0
+    var converted: Float        = 0
+    
+    var fromCurency: String     = ""
+    var toCurency: String       = ""
+    
+    var apiClient:APIHandler?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         inputField.delegate = self
+        inputField.addTarget(self, action: #selector(onChangeInput(_:)), for: .editingChanged)
+        resultLabel.text = "Result:\n\n\(converted)"
+        
+        menuTo.menu = UIMenu(children: [
+                    UIAction(title: "First", handler: toMenuAction),
+                    UIAction(title: "Second", handler: toMenuAction)
+                ])
+        menuFrom.menu = UIMenu(children: [
+                    UIAction(title: "First", handler: toMenuAction),
+                    UIAction(title: "Second", handler: toMenuAction),
+                ])
+        
+        apiClient = APIHandler()
+//        print(apiClient?.symbols ?? "no symbols from apiClient")
+        Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(menusInit), userInfo: nil, repeats: false)
         
         let tapGestureBackground = UITapGestureRecognizer(target: self, action: #selector(self.backgroundTapped(_:)))
             self.view.addGestureRecognizer(tapGestureBackground)
-        
-        //separate function to initialize the menus
-        menuTo.menu = UIMenu(children: [
-            UIAction(title: "First", handler: menuAction),
-            UIAction(title: "Second", handler: menuAction)
-        ])
-        menuFrom.menu = UIMenu(children: [
-            UIAction(title: "First", handler: menuAction),
-            UIAction(title: "Second", handler: menuAction),
-        ])
     }
     
-    @objc func backgroundTapped(_ sender: UITapGestureRecognizer)
-    {
+    @objc func menusInit() {
+        print(apiClient?.symbols ?? "")
+        
+        menuTo.menu = UIMenu(children: symbolToChildren(symbols: apiClient?.symbols ?? [], type: "to"))
+        menuFrom.menu = UIMenu(children: symbolToChildren(symbols: apiClient?.symbols ?? [], type: "from"))
+        
+        fromCurency = apiClient?.symbols.first ?? ""
+        toCurency = apiClient?.symbols.first ?? ""
+    }
+    
+    func symbolToChildren(symbols: [String], type: String) -> [UIAction] {
+        var rez: [UIAction] = []
+        
+        for symbol in symbols.sorted() {
+            if type == "to" {
+                rez.append(UIAction(title: symbol, handler: toMenuAction))
+            } else {
+                rez.append(UIAction(title: symbol, handler: fromMenuAction))
+            }
+        }
+        
+        return rez
+    }
+    
+    @objc func backgroundTapped(_ sender: UITapGestureRecognizer) {
         inputField.endEditing(true)
     }
     
-    func menuAction(action: UIAction) {
-        //setting 'from' and 'to' curencies according to action's title
-        print(action.title)
+    func toMenuAction(action: UIAction) {
+        toCurency = action.title
     }
-
-    @IBAction func inputFieldAction(_ sender: UITextField, forEvent event: UIEvent) {
-        //set the 'from' value that needs to be converted
-        print(sender.text!)
+    
+    func fromMenuAction(action: UIAction) {
+        fromCurency = action.title
+    }
+    
+    @objc func onChangeInput(_ textField: UITextField) {
+        initial = Float(textField.text ?? "0") ?? 0
+        print(initial)
     }
     
     @IBAction func convertButtonTapped(_ sender: UIButton) {
         //converting the value
+        
+        apiClient?.convert(from: fromCurency, to: toCurency, amount: initial)
+        
+        Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(showConvertedValue), userInfo: nil, repeats: false)
+    }
+    
+    @objc func showConvertedValue() {
+        resultLabel.text = "Result:\n\n\(apiClient?.convertedValue ?? 0)"
     }
 }
 
